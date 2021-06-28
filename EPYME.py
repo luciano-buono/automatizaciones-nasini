0f473654dbe04456b0fcdfd35e6014d200c50eb1
luciano-buono <luciano@DESKTOP-Lucho.localdomain>
2021-06-28 16:45:47 -0300

    Agregado archivo de Magui

---
+++ EPYME.py
@@ -0,0 +1,35 @@
+import glob
+import os
+from numpy.lib.shape_base import column_stack
+import pandas as pd
+pd.options.mode.chained_assignment = None  # default='warn'
+
+
+# list_of_files = glob.glob(r"C:\Users\danyb\OneDrive\Escritorio\Python\*.xls") # * means all if need specific format then *.csv
+list_of_files = glob.glob(r'*.xls') # * means all if need specific format then *.csv
+latest_file = max(list_of_files, key=os.path.getmtime)
+
+df = pd.read_excel(latest_file, converters={'CUIT':str, 'CUIT DEL COMITENTE': str,'SUBCUENTA COMITENTE': int, 'IMPORTE': float})
+
+
+df['1']=';;;;;;;'
+df['C']=1
+df['D']='No garantizado'
+df['E']='no'
+df['F']='no'
+df['K']='si'
+df['L']='CVSA'
+df['N']='ECHEQ'
+
+
+df2 = df[['1','CUIT','CUIT DEL COMITENTE','C','D','E','F','SUBCUENTA COMITENTE','FECHA DE EMISION','FECHA DE PAGO','IMPORTE','K','L','ID','N']] # Armado de archivo
+
+
+df2['CUIT'] = df['CUIT'].str.slice(stop=2)+'-'+df['CUIT'].str.slice(start=2)+'-'+df['CUIT'].str.slice(start=-1)
+df2['CUIT DEL COMITENTE']= df['CUIT DEL COMITENTE'].str.slice(stop=2)+'-'+df['CUIT DEL COMITENTE'].str.slice(start=2)+'-'+df['CUIT DEL COMITENTE'].str.slice(start=-1)
+df2['FECHA DE EMISION'] = pd.to_datetime(df2['FECHA DE EMISION']).dt.date
+df2['FECHA DE PAGO'] = pd.to_datetime(df2['FECHA DE PAGO']).dt.date
+
+
+df2.to_csv('EpymeExportado.csv', sep=';',header=False, index=False) #guarda en csv
+
