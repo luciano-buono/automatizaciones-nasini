6d102da6b9e080d50db837550671f2082975a6b3
luciano-buono <luciano@DESKTOP-Lucho.localdomain>
2021-06-28 17:35:13 -0300

    arreglada columnas vacias y format date

--- EPYME.py
+++ EPYME.py
@@ -11,8 +11,6 @@ latest_file = max(list_of_files, key=os.path.getmtime)
 
 df = pd.read_excel(latest_file, converters={'CUIT':str, 'CUIT DEL COMITENTE': str,'SUBCUENTA COMITENTE': int, 'IMPORTE': float})
 
-
-df['1']=';;;;;;;'
 df['C']=1
 df['D']='No garantizado'
 df['E']='no'
@@ -22,13 +20,19 @@ df['L']='CVSA'
 df['N']='ECHEQ'
 
 
-df2 = df[['1','CUIT','CUIT DEL COMITENTE','C','D','E','F','SUBCUENTA COMITENTE','FECHA DE EMISION','FECHA DE PAGO','IMPORTE','K','L','ID','N']] # Armado de archivo
+df2 = df[['CUIT','CUIT DEL COMITENTE','C','D','E','F','SUBCUENTA COMITENTE','FECHA DE EMISION','FECHA DE PAGO','IMPORTE','K','L','ID','N']] # Armado de archivo
 
 
 df2['CUIT'] = df['CUIT'].str.slice(stop=2)+'-'+df['CUIT'].str.slice(start=2)+'-'+df['CUIT'].str.slice(start=-1)
 df2['CUIT DEL COMITENTE']= df['CUIT DEL COMITENTE'].str.slice(stop=2)+'-'+df['CUIT DEL COMITENTE'].str.slice(start=2)+'-'+df['CUIT DEL COMITENTE'].str.slice(start=-1)
 df2['FECHA DE EMISION'] = pd.to_datetime(df2['FECHA DE EMISION']).dt.date
 df2['FECHA DE PAGO'] = pd.to_datetime(df2['FECHA DE PAGO']).dt.date
+
+df2['FECHA DE EMISION'] = pd.to_datetime(df2['FECHA DE EMISION']).dt.strftime('%d/%m/%Y')
+df2['FECHA DE PAGO'] = pd.to_datetime(df2['FECHA DE PAGO']).dt.strftime('%d/%m/%Y')
+
+columnas = ['1','2','3','4','5','6','7','8']
+df2 = df2.reindex(columns =  columnas +df2.columns.tolist())
 
 
 df2.to_csv('EpymeExportado.csv', sep=';',header=False, index=False) #guarda en csv
