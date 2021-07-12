1f1a4ed703c6fba376df1d1405441ebdc3378e4f
luciano-buono <luciano@DESKTOP-Lucho.localdomain>
2021-07-12 16:34:14 -0300

    Multiples inputs, concatena

--- cpd-echeq/cpd-echeq.py
+++ cpd-echeq/cpd-echeq.py
@@ -19,24 +19,51 @@ import pandas as pd
 # file = parser.parse_args()
 
 #2) usa el ultimo archivo modificado como entrada. Acepta multiples formatos
-file_extension = ['xlsx','xls']
+extensions_allowed = ['xls','xlsx']
+#Cant de inputs
+input_amount = ''
+input_amount = input("Cant de archivos de entrada? (Default:1)")
+if input_amount:
+    input_amount = int(input_amount)
+else:
+    input_amount = 1
 list_of_files = []
+file, filename, file_extension = [], [], []
 try:
-    list_of_files += glob.glob(r'*.'+file_extension[0])
-    list_of_files += glob.glob(r'*.'+file_extension[1])
-    file = max(list_of_files, key=os.path.getmtime)
-    print(f"Leyendo archivo: {file}\n ")
-    #Get the extension of file
-    filename, file_extension = os.path.splitext(file)
-    out_file_name = filename
+    #Busca todos los archivos con las extensiones pasadas
+    for i in range(len(extensions_allowed)):
+        list_of_files += glob.glob(r'*.'+extensions_allowed[i])
+    #Comprueba que input_amount no sea mayor al tamaño de la lista
+    if input_amount > len(list_of_files):
+        print(f"No hay {input_amount} files en la carpeta {os.getcwdb()}")    
+        sys.exit(1)
+    #Sorted list of latest inputs
+    sorted_files = sorted(list_of_files, key=os.path.getctime)
+    for i in range(input_amount):
+        file.append(sorted_files[-(i+1)])
+        print(f"Leyendo archivo: {file[i]}\n ")
+        #Get the extension of file and the root
+        filename.append(os.path.splitext(file[i])[0])
+        file_extension.append(os.path.splitext(file[i])[1])
+        out_file_name = filename[i]
 except ValueError as err:
-    print(f"ERROR No hay {file_extension} en el directorio actual (${os.getcwdb()})")
+    print(f"ERROR No hay {file_extension} en el directorio actual ({os.getcwdb()})")
     sys.exit(1)
 
-##MAIN
-df = pd.read_excel(file, skiprows=1)
+#MAIN
+print(file[0])
+df = pd.read_excel(file[0], skiprows=1)
 df2 = df[df['Información'].str.contains("Subasta")]
 df3 = df2.groupby(['Cuenta']).sum()
+#Segundo dataframe/archivo
+if input_amount > 1:
+    print(file[1])
+    df4 = pd.read_excel(file[1], skiprows=1)
+    df5 = df4[df4['Información'].str.contains("Subasta")]
+    df6 = df5.groupby(['Cuenta']).sum()
+    #Unimos ambos dataframes
+    frames = [df3, df6]
+    df3 = pd.concat(frames)
 #Guardamos el csv
 print(f'Output: {out_file_name}-edited.csv ')
 df3.to_csv(out_file_name+'-edited.csv', sep=';',header=True, index=True, decimal=',') #guarda en csv
